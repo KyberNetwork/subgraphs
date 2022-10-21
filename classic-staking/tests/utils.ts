66bca3922fd9be0718ba59a7e7161fc9b9b0a3b3
Hải Nam <lehainam.dev@gmail.com>
2022-10-21 15:38:57 +0700

    test: add test for fairLaunch event handler

---
+++ classic-staking/tests/utils.ts
@@ -0,0 +1,65 @@
+import { newMockEvent } from 'matchstick-as'
+import { ethereum, Address, BigInt } from '@graphprotocol/graph-ts'
+import { AddNewPool, Deposit, Withdraw } from '../src/types/templates/KyberFairLaunch/KyberFairLaunch'
+
+export const DUMMY_ADDRESS = '0x0000000000000000000000000000000000000001'
+
+export function createAddNewPoolEvent(
+  address: Address,
+  stakeToken: Address,
+  startBlock: i32,
+  endBlock: i32,
+  rewardPerBlocks: ethereum.Value[]
+): AddNewPool {
+  let event = changetype<AddNewPool>(newMockEvent())
+
+  event.address = address
+  event.parameters = new Array()
+
+  event.parameters.push(new ethereum.EventParam('stakeToken', ethereum.Value.fromAddress(stakeToken)))
+  event.parameters.push(new ethereum.EventParam('startBlock', ethereum.Value.fromI32(startBlock)))
+  event.parameters.push(new ethereum.EventParam('endBlock', ethereum.Value.fromI32(endBlock)))
+  event.parameters.push(new ethereum.EventParam('rewardPerBlocks', ethereum.Value.fromArray(rewardPerBlocks)))
+
+  return event
+}
+
+export function createDepositEvent(
+  address: Address,
+  user: Address,
+  pid: BigInt,
+  blockNumber: BigInt,
+  amount: BigInt
+): Deposit {
+  let event = changetype<Deposit>(newMockEvent())
+
+  event.address = address
+  event.parameters = new Array()
+
+  event.parameters.push(new ethereum.EventParam('user', ethereum.Value.fromAddress(user)))
+  event.parameters.push(new ethereum.EventParam('pid', ethereum.Value.fromUnsignedBigInt(pid)))
+  event.parameters.push(new ethereum.EventParam('blockNumber', ethereum.Value.fromUnsignedBigInt(blockNumber)))
+  event.parameters.push(new ethereum.EventParam('amount', ethereum.Value.fromUnsignedBigInt(amount)))
+
+  return event
+}
+
+export function createWithdrawEvent(
+  address: Address,
+  user: Address,
+  pid: BigInt,
+  blockNumber: BigInt,
+  amount: BigInt
+): Withdraw {
+  let event = changetype<Withdraw>(newMockEvent())
+
+  event.address = address
+  event.parameters = new Array()
+
+  event.parameters.push(new ethereum.EventParam('user', ethereum.Value.fromAddress(user)))
+  event.parameters.push(new ethereum.EventParam('pid', ethereum.Value.fromUnsignedBigInt(pid)))
+  event.parameters.push(new ethereum.EventParam('blockNumber', ethereum.Value.fromUnsignedBigInt(blockNumber)))
+  event.parameters.push(new ethereum.EventParam('amount', ethereum.Value.fromUnsignedBigInt(amount)))
+
+  return event
+}
---
+++ classic-staking/tests/v1.test.ts
@@ -0,0 +1,71 @@
+import { assert, describe, test, beforeAll, afterAll } from 'matchstick-as/assembly/index'
+import { KyberFairLaunch } from '../src/types/schema'
+import { handleAddNewPool, handleDeposit, handleWithdraw } from '../src/mappings/fairLaunch'
+import { store, ethereum, Address, BigInt } from '@graphprotocol/graph-ts'
+import { DUMMY_ADDRESS, createAddNewPoolEvent, createDepositEvent, createWithdrawEvent } from './utils'
+
+describe('handleAddNewPool()', () => {
+  beforeAll(() => {
+    let fairLaunch = new KyberFairLaunch(DUMMY_ADDRESS)
+    fairLaunch.stakeTokens = []
+    fairLaunch.save()
+  })
+
+  test('should add new pool', () => {
+    assert.entityCount('KyberFairLaunch', 1)
+    let addNewPoolEvent = createAddNewPoolEvent(
+      Address.fromString(DUMMY_ADDRESS),
+      Address.fromString(DUMMY_ADDRESS),
+      0,
+      0,
+      []
+    )
+    handleAddNewPool(addNewPoolEvent)
+
+    assert.entityCount('KyberFairLaunch', 1)
+    let fairLaunch = KyberFairLaunch.load(DUMMY_ADDRESS)
+    assert.assertNotNull(fairLaunch)
+    assert.assertTrue(fairLaunch!.stakeTokens.length === 1)
+  })
+
+  afterAll(() => {
+    store.remove('KyberFairLaunch', DUMMY_ADDRESS)
+  })
+})
+
+describe('handleDeposit() & handleWithdraw()', () => {
+  beforeAll(() => {
+    let fairLaunch = new KyberFairLaunch(DUMMY_ADDRESS)
+    fairLaunch.stakeTokens = [Address.fromString(DUMMY_ADDRESS)]
+    fairLaunch.save()
+  })
+
+  test('should handle amount correctly', () => {
+    assert.entityCount('StakingPosition', 0)
+    const pid = BigInt.fromI32(0)
+    let depositEvent = createDepositEvent(
+      Address.fromString(DUMMY_ADDRESS),
+      Address.fromString(DUMMY_ADDRESS),
+      pid,
+      BigInt.fromI32(0),
+      BigInt.fromI32(10000)
+    )
+    let withdrawEvent = createWithdrawEvent(
+      Address.fromString(DUMMY_ADDRESS),
+      Address.fromString(DUMMY_ADDRESS),
+      pid,
+      BigInt.fromI32(0),
+      BigInt.fromI32(1234)
+    )
+
+    handleDeposit(depositEvent)
+    handleWithdraw(withdrawEvent)
+
+    assert.entityCount('StakingPosition', 1)
+    assert.fieldEquals('StakingPosition', `${DUMMY_ADDRESS}-${DUMMY_ADDRESS}-${pid}`, 'amount', '8766')
+  })
+
+  afterAll(() => {
+    store.remove('KyberFairLaunch', DUMMY_ADDRESS)
+  })
+})
